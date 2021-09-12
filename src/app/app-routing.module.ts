5af4cbb7c25e644907308c99a9014bff6cb0714c
paulsaver <paul.sav21@gmail.com>
2021-09-13 00:38:48 +0400

    Add icons and credits page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { CreditComponent } from './credit/credit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
   { path: 'products', component: ProductListComponent},
   { path: 'detail/:id', component: ProductComponent},
-  { path: 'add', component: AddProductComponent}
+  { path: 'add', component: AddProductComponent},
+  { path: 'credits', component: CreditComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { CreditComponent } from './credit/credit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductComponent,
-    AddProductComponent
+    AddProductComponent,
+    CreditComponent
   ],
   imports: [
     BrowserModule,
